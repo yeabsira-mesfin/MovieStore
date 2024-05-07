45d039544aa59bad3399c84bb6e8147ceeb47697
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-05-07 06:26:35 -0400

    movie list

--- movie-store/src/Components/MovieList.jsx
+++ movie-store/src/Components/MovieList.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 
 const MovieList = () => {
+
+    const movies = [
+        {id:1, title: 'Titanic', genre: 'Romance', year: 1997, rating:7.9},
+        {id:2, title: 'Fired Up', genre: 'Comedy', year: 2009, rating:6.1},
+        {id:2, title: 'Django Unchained', genre: 'Action', year: 2012, rating:8.5},
+        {id:2, title: 'Grey Man', genre: 'Action', year: 2022, rating:6.5},
+        {id:2, title: 'Wedding Crashers', genre: 'Romantic Comedy', year: 2005, rating:7},
+        {id:2, title: 'The Notebook', genre: 'Romantic Comedy', year: 2004, rating:7.8},
+    ];
   return (
     <div>MovieList</div>
   )
